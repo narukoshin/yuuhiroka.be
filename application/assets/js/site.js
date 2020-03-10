efcf0459dce2bb04292e07b744a937b44e872d7c
unknown <alekss273@inbox.lv>
2020-03-10 18:38:02 +0200

    Added navigation

--- application/assets/js/site.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        var test = $(this).scrollTop()
-        
-    })
-    $(window).scroll(function(){
-        $(window).closest('section').css('background-color', 'red')
-    })
-})
