01ceec91a1c11d3bc992490dd4c1f0800ded7826
unknown <alekss273@inbox.lv>
2020-03-24 01:10:45 +0200

    created language controller

---
+++ application/controllers/language.php
@@ -0,0 +1,83 @@
+<?php
+    /**
+     * @author Yuu Hirokabe
+     * @version 1.0.0
+     * @copyright www.yuuhiroka.be 2020
+     */
+    class Language{
+        /**
+         * @var string current language
+         */
+        private static $currentLanguage;
+        /**
+         * @var string default language
+         */
+        private static $defaultLanguage = 'en';
+        /**
+         * @var array translated content
+         */
+        private static $translate;
+        /**
+         * @return void
+         */
+        public function __construct(){
+            self::$currentLanguage = $_COOKIE['language'] ?? self::$defaultLanguage;
+        }
+        /**
+         * Setting language to english
+         * 
+         * @return void
+         */
+        public static function en(){
+            setcookie('language', 'en', time()+60);
+        }
+        /**
+         * Setting language to latvian
+         * 
+         * @return void
+         */
+        public static function lv(){
+            setcookie('language', 'lv', time()+60);
+        }
+        /**
+         * Setting language to russian
+         * 
+         * @return void
+         */
+        public static function ru(){
+            setcookie('language', 'ru', time()+60);
+        }
+        /**
+         * Setting language to japanese
+         * 
+         * @return void
+         */
+        public static function ja(){
+            setcookie('language', 'ja', time()+60);
+        }
+        /**
+         * Returning current language
+         * 
+         * @return string
+         */
+        public static function current(){
+            return self::$currentLanguage;
+        }
+        /**
+         * Translating site content
+         * 
+         * @param array $lang
+         * @return void
+         */
+        public static function translate(array $lang){
+            self::$translate = $lang;
+        }
+        /**
+         * Returning translated content to views
+         * 
+         * @return array
+         */
+        public static function test2(){
+            return self::$translate;
+        }
+    }
