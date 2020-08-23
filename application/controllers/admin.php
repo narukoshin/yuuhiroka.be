d191dd69be55850f491f1befe954b1cf5155bdae
Yuuko Shionji <alekss273@inbox.lv>
2020-08-24 01:47:42 +0300

    updated make_auth function

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -45,16 +45,48 @@
             self::make_auth();
         }
         /**
-         * Making auth
+         * Authorization algo
          * 
          * @return void
          */
         private static function make_auth(){
+            // Getting values from login form
             $body        = (object)app('request')->body;
             $username    = $body->username;
-            $password    = $body->password;
+            $passwd      = $body->password;
+            $db          = Database::getConnection();
+            // Sql statament
+            $stmt        = $db->prepare('SELECT `id`, `password`, `email` FROM `accounts` WHERE `username` = ? LIMIT 1;');
+            // Sending sql statament to server
+            $result      = $stmt->execute([$username]);
+            // If sql statament was successfuly executed
+            if ($result){
+               // If username is valid and exists in database   
+               if ($stmt->rowCount()){
+                    // Extracting variables from query
+                    extract($stmt->fetch());
+                     // Checking, if user entered password match to password in database
+                     // If password is valid
+                    if (password_verify($passwd, $password)){
+                        // Creates user auth session
+                        static::createSession($db, $username, $passwd, time());
 
-            echo $password;
+                        // Updates user last login time
+                        static::updateUserLastLogin($db, $id);
+
+                        // Redirecting user to dashboard
+                        return static::redirect('admin/dashboard');
+                     // If password is invalid
+                    } else {
+                        // Returning login page with invalid password error
+                        return self::index(['{login.error}' => 'You entered wrong password, please try again..']);
+                    }
+                 // If username is invalid
+               } else {
+                    // Returning login page with invalid username error
+                    return self::index(['{login.error}' => "Sorry, we can't find account with that username."]);
+               }
+            }
         }
         /**
          * Dashboard
